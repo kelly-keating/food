a9e7dceb14689bfa5a54cc8eaa43a666a332b0a1
kelly-keating <keating.kel@gmail.com>
2017-05-05 11:49:32 +1200

    home page handlebars added

--- routes.js
+++ routes.js
@@ -4,7 +4,9 @@ var fs = require('fs')
 
 module.exports = router
 
+var tempData = [{title:'Cake', imgURL:'http://matthewpalmer.net/rocket/icon.png'}, {title:'Dessert', imgURL:'http://matthewpalmer.net/rocket/icon.png'}, {title:'Pies', imgURL:'http://matthewpalmer.net/rocket/icon.png'}]
+
 router.get('/', (req, res) => {
   console.log("rendering home");
-  res.render('home')
+  res.render('home', {tempData})
 })
