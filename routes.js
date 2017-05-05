94aa6b8711c514b8c3321e029ab1d45382a2b2d8
kelly-keating <keating.kel@gmail.com>
2017-05-05 14:25:44 +1200

    post harrison

--- routes.js
+++ routes.js
@@ -5,30 +5,20 @@ var recipes = require('./recipes.json')
 
 module.exports = router
 
-var tempData = [{name:'Cake', imgUrl:'http://matthewpalmer.net/rocket/icon.png'}, {name:'Dessert', imgUrl:'http://matthewpalmer.net/rocket/icon.png'}, {name:'Pies', imgUrl:'http://matthewpalmer.net/rocket/icon.png'}]
-
 router.get('/', (req, res) => {
-  // console.log(req.query);
-  // if (Object.keys(req.query).length != 0) { //there is no query
-  //   res.render('query') //give filtered results
-  // } else {
-  //   console.log("rendering home");
-    res.render('home') //give all results
+    res.render('home')
 })
-
-// router.get('/cakes/:id', (req, res) => {
-//   var cakes = recipes.foods.find((cakes)=> req.params.id == cakes.id)
-//   console.log(cakes);
-//   res.render('home', {cakes})
-// })
 
 
 router.get('/cakes/:id', (req, res) => {
   var id = req.params.id
-  recipes.find(function(recipe) {
-    if (recipe.id == id) {
-      res.render('showcake', recipe)
-    }
+  console.log(recipes);
+  var recipe = recipes.find(function(recipe) {
+   return recipe.id == id
   })
-
+  if (recipe) {
+    res.render('showcake', recipe)
+  } else {
+    res.send('couldnt find it')
+  }
 })
